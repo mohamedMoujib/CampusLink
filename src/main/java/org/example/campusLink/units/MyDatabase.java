e9c51ff69f34555823075fdad6c259c790ce8ec7
MoujibHaouech <haouech.mohamedmoujib@gmail.com>
2026-02-05 13:30:18 +0100

    Singleton added

--- src/main/java/org/example/campusLink/units/MyDatabase.java
+++ src/main/java/org/example/campusLink/units/MyDatabase.java
@@ -1,4 +1,35 @@
 package org.example.campusLink.units;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class MyDatabase {
+    private final String URL = "jdbc:mysql://localhost:3306/campusLink";
+    private final String USER = "root";
+    private final String PASSWORD = "root";
+    private Connection connection;
+    private static MyDatabase instance;
+
+    private MyDatabase() {
+        try {
+            this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/campusLink", "root", "root");
+            System.out.println("Connection established");
+        } catch (SQLException e) {
+            throw new RuntimeException("Error connecting to database", e);        }
+
+    }
+
+    public static MyDatabase getInstance() {
+        if (instance == null) {
+            instance = new MyDatabase();
+        }
+
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return this.connection;
+    }
 }
+
